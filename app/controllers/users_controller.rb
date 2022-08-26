aa951a9c424477072b5c5c71722099bcc19aee4c
EC2 Default User <ec2-user@ip-172-31-30-161.ap-northeast-1.compute.internal>
2022-08-26 09:44:27 +0000

    [Fix]他ユーザー情報編集画面へ移動不可

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :index, :show]
-  
+
   def index
     @users = User.all
     @user = current_user
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
